932a1b19f0193879e3514467522055b637e32e5d
luiz <luiz308araujofarias@gmail.com>
2025-02-16 17:45:09 -0300

    Implementações e Ajustes

---
+++ src/js/index.js
@@ -0,0 +1,116 @@
+//EFEITO HEADER//
+let lastScroll = 0
+
+window.addEventListener('scroll', () => {
+    let header = document.querySelector('header')
+    const currentScroll = window.scrollY
+
+    if(currentScroll > lastScroll){
+        header.classList.add('-translate-y-28')
+    }
+    else{
+        header.classList.remove('-translate-y-28')
+    }
+    lastScroll = currentScroll
+})
+
+//TELA DE CARREGAMENTO//
+window.onload = function() {
+    let loadScreen = document.querySelector('#load-screen')
+    let loadImage = document.querySelector('#load-img')
+    let loadBar = document.querySelector('#load-bar')
+    let body = document.querySelector('body')
+    
+    //TEMPO LIMITE//
+    setTimeout(() =>{
+        loadScreen.classList.add('w-0')
+        
+        body.classList.remove('overflow-hidden')
+        loadImage.classList.add('opacity-0')
+        loadBar.classList.add('opacity-0')
+
+    },2000)
+}
+
+//BARRA DE OPÇOES//
+
+const clickDivs = document.querySelectorAll('[data-target]')
+
+//FUNÇÃO PARA CADA DIV//
+clickDivs.forEach(div => {
+    div.addEventListener('click', () => {
+
+    //PEGA O ID DO CONTIANER ASSOCIADO//
+    const targetId = div.getAttribute('data-target')
+
+    //ESCONDE TODOS OS CONTAINERS//
+    document.querySelectorAll('[id^="container"]').forEach(container => {
+        container.classList.add('faded')
+    })
+
+    //MOSTRA O BOTÃO CLICADO AO CONTAINER ASSOCIADO//
+    const targetContainer = document.getElementById(targetId)
+
+    if (targetContainer) {
+        targetContainer.classList.remove('faded');
+    }
+    })
+})
+
+//SLIDER PROJETOS//
+const slides = document.querySelector('.slides')
+const prevButton = document.querySelector('.prev')
+const nextButton = document.querySelector('.next')
+let currentIndex = 0//VARIAVEL CONTADOR//
+const totalSlides = document.querySelectorAll('.slide').length
+
+//FUNÇÃO PARA PASSAR AS IMAGENS DO SLIDESHOW//
+function showSlide(index) {
+    const slideWidth = document.querySelector('.slide').clientWidth
+    slides.style.transform = `translateX(-${index * slideWidth}px)`
+}
+//FUNÇÃO DE CLICK PARA BOTAO PROXIMO//
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % totalSlides
+    showSlide(currentIndex)
+})
+//FUNÇÃO DE CLICK PARA BOTAO VOLTAR//
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides
+    showSlide(currentIndex)
+})
+//AUTOMATIZAÇÃO DO SLIDESHOW//
+setInterval(() => {
+    nextButton.click()
+},5000)
+//DARK/WHITE MODE//
+
+let modeBar = document.querySelector('.modebar')//BARRA DE MODO
+let mode = document.querySelector('.mode')//BOTÃO DE MODO
+let modeHead = document.querySelector('.modehead')
+let header = document.querySelector('header')//HEADER
+let texts = document.querySelectorAll('.texts')//TODOS OS TEXTOS
+let section = document.querySelectorAll('.container')//TODAS AS SESSOES
+let slideArticle = document.querySelectorAll('.slide article')
+let html = document.querySelector('html')//CHAMA HTML
+
+modeBar.addEventListener('click', () => {
+    mode.classList.toggle('bg-white')
+    mode.classList.toggle('translate-x-[150%]')
+    modeHead.classList.toggle('max-sm:bg-white')
+
+    header.classList.toggle('bg-white')
+    html.classList.toggle('dark')
+
+    //PARA CADA ESCRITA//
+    texts.forEach(texts => {
+        texts.classList.toggle('dark:text-black')
+    })
+    slideArticle.forEach(slideArticle => {
+        slideArticle.classList.toggle('bg-whitefade')
+    })
+    //PARA CADA SESSAO//
+    section.forEach(section => {
+        section.classList.toggle('bg-white')
+    })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,201 @@
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  important: true,
+  mode: 'jit',
+  content: ["./**/*.html", "./src/**/*.{html,js,jsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        white: '#fff',
+        black: '#000',
+        blackfade: 'rgba(0, 0, 0, 0.9)',
+        whitefade: 'rgba(255, 255, 255, 0.9)',
+        zero: '#ecce7a',
+        one: '#d3b561',
+        two:'#c0a450',
+        three: '#ad923f',
+        four: '#99812d',
+        five: '#4a2512',
+      },
+      backgroundImage: {
+        'banner-pc': "url('/src/assets/banner/pc.webp')",
+      },
+      height: {
+        '150': '150vh',
+        '200': '200vh',
+        '220': '220vh',
+      },
+      zIndex: {
+        '1': '1',
+        '2': '2',
+        '3': '3',
+      },
+      fontFamily: {
+        cinzel: ['Cinzel, serif'],
+        afacad: ['Afacad Flux, sans-serif'],
+      },
+      width: {
+        'w-zero': '0', 
+      },
+      animation: {
+        'spin': 'spinbar 1s linear infinite',
+        'float': 'floating 1s infinite alternate',
+        'hoverfloat': 'hfloat .5s infinite alternate',
+        'words': 'words 20s steps(10) infinite',
+        'wordstwo': 'wordstwo 20s steps(10) infinite',
+        'bar': 'bar .5s infinite',
+      },
+      keyframes: {
+        spinbar: {
+          '0%': {transform:'rotate(0deg)'},
+          '100%': {transform: 'rotate(360deg)'},
+        },
+        floating: {
+          '0%': {transform:'translateY(3%)'},
+          '100%': {transform:'translateY(-3%)'},
+        },
+        hfloat: {
+          '0%': {transform:'translateY(3%)'},
+          '100%': {transform:'translateY(-3%)'},
+        },
+        words: {
+          '10%, 15%, 30%, 35%, 50%, 55%, 70%, 75%, 90%, 95%': { width: '0' },
+          '5%, 20%, 25%, 40%, 45%, 60%, 65%, 80%, 85%': {width: '11ch'},
+        },
+        wordstwo: {
+          '10%, 15%, 30%, 35%, 50%, 55%, 70%, 75%, 90%, 95%': { width: '0' },
+          '5%, 20%, 25%, 40%, 45%, 60%, 65%, 80%, 85%': {width: '8ch'},
+        },
+        bar: {
+          '0%': {borderRight: '#000'},
+        },
+        textShadow: {
+          sm: '1px 3px 3px #000'
+        },
+    },
+    transitionTimingFunction: {
+      'custom-ease': 'cubic-bezier(0.5, 1.6, 0.4, 0.7)',
+    },
+  },
+},
+  plugins: [
+    function({ addComponents }) {
+      addComponents({
+        '.container': {
+          width: '100%',
+          maxWidth: '100%', 
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          position: 'relative',
+          overflow: 'hidden',
+        },
+        '.cards': {
+          width: '300px',
+          height: '300px',
+          background: '#d3b561',
+          cursor: 'pointer',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'start',
+          flexDirection: 'column',
+          gap: '10px',
+          padding: '10px',
+          transition:'.5s',
+        },
+        '.cards:hover': {
+          scale: '1.08',
+        },
+        '.icons': {
+          width: '65px',
+        },
+        '.flex-box': {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        '.flex-box-column': {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        },
+        '.topico': {
+          color:'#d3b561',
+          position: 'relative',
+        },
+        '.topico::before': {
+          content: '""',
+          position: 'absolute',
+          width: '0%',
+          height: '1px',
+          bottom: '0',
+          left: '0',
+          background: '#d3b561',
+        },
+        '.topico:hover::before': {
+          width: '100%',
+        },
+        '.faded': {
+          transform: 'translateY(50%)',
+          opacity: '0',
+          zIndex: '-1',
+        },
+        '.content-box': {
+          width: '35%',
+          height: '85%',
+          border: '1px solid #d3b561',
+          borderRadius: '10px',
+          gap: '2px',
+          position: 'relative',
+          background: 'transparent',
+          transition: '.5s',
+        },
+        '.slide': {
+          position: 'relative',
+          width: '100%',
+          height: '100vh',
+          flexShrink: '0',
+        },
+        '.slide-img': {
+          width: '100%',
+          height: '75%',
+          overflow: 'hidden',
+          objectFit: 'cover',
+          position: 'absolute',
+          top: '0%'
+        },
+        '.scrollbar': {
+          '&::-webkit-scrollbar': {
+            width: '10px',
+            height: '100%',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: '#000',
+            cursor: 'pointer',
+          },
+          '&::-webkit-scrollbar-track': {
+            backgroundColor: 'transparent',
+          },
+        },
+        '.scrollbar-body': {
+          '&::-webkit-scrollbar': {
+            width: '10px',
+            height: '100%',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: '#ecce7a',
+          },
+          '&::-webkit-scrollbar-track': {
+            backgroundColor: '#000000',
+          },
+        },
+      });
+    },
+  ],
+};
